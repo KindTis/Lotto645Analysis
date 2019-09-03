6ba58d5de0533c22916a34afc35fc230261be1e4
tatis3 <tatis3@nexon.co.kr>
2019-09-03 16:29:22 +0900

    웹에서 로또 당첨 번호를 파싱하도록 기능 추가

--- Lotto645Analysis.py
+++ Lotto645Analysis.py
@@ -1,83 +1,109 @@
 import xlrd
 import operator
+import random
+import time
+import urllib.request
+from bs4 import BeautifulSoup
 
 class Lotto645Analysis:
-    def __init__(self, file):
-        self.lottoWins = []
-        self.ParseLottoWins(file)
+    def __init__(self):
+        self.winLottos = {}
+        self.winLottosSum = {}
+        self.DownloadLottoResults()
 
-    def ParseLottoWins(self, file):
-        workbook = xlrd.open_workbook(file)
-        sheet = workbook.sheet_by_index(0)
-        print("총 회차", sheet.nrows - 3)
-        for i in reversed(range(sheet.nrows)):
-            if i == 2:
-                break
+    def DownloadLottoResults(self):
+        url = 'https://dhlottery.co.kr/gameResult.do?method=allWinExel&gubun=byWin&nowPage=&drwNoStart=1&drwNoEnd=9999'
+        urllib.request.urlretrieve(url, 'lottoResult.html')
 
-            lottoWin = []
-            lottoWin.append(int(sheet.cell_value(i, 13)))
-            lottoWin.append(int(sheet.cell_value(i, 14)))
-            lottoWin.append(int(sheet.cell_value(i, 15)))
-            lottoWin.append(int(sheet.cell_value(i, 16)))
-            lottoWin.append(int(sheet.cell_value(i, 17)))
-            lottoWin.append(int(sheet.cell_value(i, 18)))
-            lottoWin.append(int(sheet.cell_value(i, 19)))
-            self.lottoWins.append(lottoWin)
+        self.winLottos.clear()
+        with open('lottoResult.html') as fp:
+            soup = BeautifulSoup(fp, 'html.parser')
+            table = soup.find_all('table')
+            numbersTable = table[1]
+            for winNumbers in numbersTable.children:
+                if winNumbers == '\n':
+                    continue
+                if winNumbers.find(string='5,000원') == None and winNumbers.find(string='10,000원') == None:
+                    continue
 
-    def CompareMyLotto(self, myLotto):
-        for i in range(len(self.lottoWins)):
+                winLottoNumbers = []
+                if len(winNumbers.contents) == 41:
+                    key = int(winNumbers.contents[3].text)
+                    winLottoNumbers.append(int(winNumbers.contents[27].text))
+                    winLottoNumbers.append(int(winNumbers.contents[29].text))
+                    winLottoNumbers.append(int(winNumbers.contents[31].text))
+                    winLottoNumbers.append(int(winNumbers.contents[33].text))
+                    winLottoNumbers.append(int(winNumbers.contents[35].text))
+                    winLottoNumbers.append(int(winNumbers.contents[37].text))
+                    winLottoNumbers.append(int(winNumbers.contents[39].text))
+                else:
+                    key = int(winNumbers.contents[1].text)
+                    winLottoNumbers.append(int(winNumbers.contents[25].text))
+                    winLottoNumbers.append(int(winNumbers.contents[27].text))
+                    winLottoNumbers.append(int(winNumbers.contents[29].text))
+                    winLottoNumbers.append(int(winNumbers.contents[31].text))
+                    winLottoNumbers.append(int(winNumbers.contents[33].text))
+                    winLottoNumbers.append(int(winNumbers.contents[35].text))
+                    winLottoNumbers.append(int(winNumbers.contents[37].text))
+                self.winLottos[key] = winLottoNumbers
+
+        print('총 {0}회'.format(len(self.winLottos)))
+
+    def CompareMyLotto(self, myNumber):
+        for key in self.winLottos:
             matchCnt = 0
-            lottoWin = self.lottoWins[i]
-            for j in range(len(myLotto)):
-                for k in range(len(lottoWin) - 1):
-                    if myLotto[j] == lottoWin[k]:
+            winNumber = self.winLottos[key]
+            for j in range(len(myNumber)):
+                for k in range(len(winNumber) - 1):
+                    if myNumber[j] == winNumber[k]:
                         matchCnt += 1
 
             if matchCnt == 6:       # 1등
-                print(i + 1, 1)
+                print(key, 1)
             elif matchCnt == 5:     # 2, 3등
                 is2nd = False
-                for j in range(len(myLotto)):
-                    if myLotto[j] == lottoWin[6]:
+                for j in range(len(myNumber)):
+                    if myNumber[j] == winNumber[6]:
                         is2nd = True
-                        print(i + 1, 2)
+                        print(key, 2)
                 if is2nd == False:
-                    print(i + 1, 3)
+                    print(key, 3)
             elif matchCnt == 4:     # 4등
-                print(i + 1, 4)
+                print(key, 4)
             elif matchCnt == 3:     # 5등
-                print(i + 1, 5)
+                print(key, 5)
 
     def CompareWithEachWinNumbers(self):
-        for i in range(len(self.lottoWins)):
-            srcLotto = self.lottoWins[i]
-            for j in range(len(self.lottoWins)):
-                if i == j:
+        for srckey in self.winLottos:
+            srcLotto = self.winLottos[srckey]
+            for dstkey in self.winLottos:
+                if srckey == dstkey:
                     continue
 
                 matchCnt = 0
-                dstLotto = self.lottoWins[j]
+                dstLotto = self.winLottos[dstkey]
 
-                for k in range(len(srcLotto) - 1):
-                    for l in range(len(dstLotto) - 1):
-                        if srcLotto[k] == dstLotto[l]:
+                for i in range(len(srcLotto) - 1):
+                    for j in range(len(dstLotto) - 1):
+                        if srcLotto[i] == dstLotto[j]:
                             matchCnt += 1
 
                 if matchCnt == 6:       # 1등
-                    print(i + 1, j + 1, 1)
+                    print(srckey, dstkey, 1)
                 elif matchCnt == 5:     # 2, 3등
                     is2nd = False
                     for k in range(len(srcLotto) - 1):
                         if srcLotto[k] == dstLotto[6]:
                             is2nd = True
-                            print(i + 1, j + 1, 2)
+                            print(srckey, dstkey, 2)
                     if is2nd == False:
-                        print(i + 1, j + 1, 3)
+                        print(srckey, dstkey, 3)
 
-    def SumWinNumbers(self):
+    def InitSumWinNumbers(self, cv):
+        self.winLottosSum.clear()
         l = {}
-        for i in range(len(self.lottoWins)):
-            srcLotto = self.lottoWins[i]
+        for key in self.winLottos:
+            srcLotto = self.winLottos[key]
             sum = 0
             for j in range(len(srcLotto) - 1):
                 sum += srcLotto[j]
@@ -85,11 +111,47 @@ class Lotto645Analysis:
                 l[str(sum)] = 0
             l[str(sum)] += 1
         
-        print(sorted(l.items(), key=lambda kv: kv[1], reverse=True))
+        l = sorted(l.items(), key=lambda kv: kv[1], reverse=True)
+
+        for sum in l:
+            if sum[1] >= cv:
+                self.winLottosSum[sum[0]] = sum[1]
+            
+    def GenNumFromSumWinNumbers(self, min, max, len, cnt):
+        _cnt = 0
+        while True:
+            pickNum = []
+            sum = 0
+            for i in range(0, len):
+                newNum = -1
+                while True:
+                    time.sleep(random.uniform(0, 3))
+                    random.seed()
+                    newNum = random.randrange(min, max + 1)
+   
+                    isDuplicateNum = False
+                    for j in pickNum:
+                        if j == newNum:
+                            isDuplicateNum = True
+                            break
+                    if isDuplicateNum == False:
+                        break
+                pickNum.append(newNum)
+
+            for num in pickNum:
+                sum += num
+
+            if str(sum) in self.winLottosSum:
+                _cnt += 1
+                print(pickNum)
+
+            if _cnt == cnt:
+                break
 
 
 if __name__ == "__main__":
-    lotto = Lotto645Analysis('lotto.xls')
-    #lotto.CompareMyLotto([1,2,3,4,5,6])
+    lotto = Lotto645Analysis()
+    lotto.CompareMyLotto([1, 2, 3, 4, 5, 6])
     #lotto.CompareWithEachWinNumbers()
-    lotto.SumWinNumbers()
+    #lotto.InitSumWinNumbers(10)
+    #lotto.GenNumFromSumWinNumbers(1, 45, 6, 5)
