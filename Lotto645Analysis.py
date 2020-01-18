13762637dd4e3cfc95541d5d2bb174506aa21db9
KindTis <tatis3@gmail.com>
2020-01-18 22:10:09 +0900

    코드 정리 및 번호뽑기에서 1, 2등 제외 조건 추가

--- Lotto645Analysis.py
+++ Lotto645Analysis.py
@@ -26,9 +26,11 @@ class Lotto645Analysis:
             for winNumbers in numbersTable.children:
                 if winNumbers == '\n':
                     continue
-                if winNumbers.find(string='5,000원') == None and winNumbers.find(string='10,000원') == None:
+                if winNumbers.find(
+                        string='5,000원') == None and winNumbers.find(
+                            string='10,000원') == None:
                     continue
-    
+
                 winLottoNumbers = []
                 if len(winNumbers.contents) == 41:
                     key = int(winNumbers.contents[3].text)
@@ -63,8 +65,9 @@ class Lotto645Analysis:
             if str(sum) not in self.winLottosSum:
                 self.winLottosSum[str(sum)] = 0
             self.winLottosSum[str(sum)] += 1
-        
-        self.winLottosSumSorted = sorted(self.winLottosSum.items(), key=lambda kv: kv[1], reverse=True)
+
+        self.winLottosSumSorted = sorted(
+            self.winLottosSum.items(), key=lambda kv: kv[1], reverse=True)
 
     def PrintWinLottoSums(self):
         for sum in self.winLottosSumSorted:
@@ -72,7 +75,7 @@ class Lotto645Analysis:
 
     def CompareWithWinLottos(self, myNumber):
         prize = [0, 0, 0, 0, 0]
-            
+
         for key in self.winLottos:
             matchCnt = 0
             winNumber = self.winLottos[key]
@@ -81,9 +84,9 @@ class Lotto645Analysis:
                     if myNumber[j] == winNumber[k]:
                         matchCnt += 1
 
-            if matchCnt == 6:       # 1등
+            if matchCnt == 6:  # 1등
                 prize[0] += 1
-            elif matchCnt == 5:     # 2, 3등
+            elif matchCnt == 5:  # 2, 3등
                 is2nd = False
                 for j in range(len(myNumber)):
                     if myNumber[j] == winNumber[6]:
@@ -91,9 +94,9 @@ class Lotto645Analysis:
                         prize[1] += 1
                 if is2nd is False:
                     prize[2] += 1
-            elif matchCnt == 4:     # 4등
+            elif matchCnt == 4:  # 4등
                 prize[3] += 1
-            elif matchCnt == 3:     # 5등
+            elif matchCnt == 3:  # 5등
                 prize[4] += 1
 
         return prize
@@ -101,23 +104,35 @@ class Lotto645Analysis:
     def PrintMyLottoPrizeHistory(self, myNumber, printShort=True):
         print('역대 1등 번호와 비교')
         print('내 번호', myNumber)
-        prizeHistory = {'1st':0, '2nd':0, '3rd':0, '4th':0, '5th':0}
+        prizeHistory = {'1st': 0, '2nd': 0, '3rd': 0, '4th': 0, '5th': 0}
 
         prize = self.CompareWithWinLottos(myNumber)
-        print('1등 {0}, 2등 {1}, 3등 {2}, 4등 {3}, 5등 {4}'.format(prize[0], prize[1], prize[2], prize[3], prize[4]))
-        
+        print('1등 {0}, 2등 {1}, 3등 {2}, 4등 {3}, 5등 {4}'.format(
+            prize[0], prize[1], prize[2], prize[3], prize[4]))
+
         myLottoSum = 0
         for i in myNumber:
             myLottoSum += i
         if str(myLottoSum) in self.winLottosSum:
             print('로또합 횟수', myLottoSum, self.winLottosSum[str(myLottoSum)])
-            
+
         print('--------------------------')
 
     def CompareWithEachWinNumbers(self):
-        analysis = {'510': 0, '515': 0, '520': 0, '525': 0, '530': 0, '410': 0, '415': 0, '420': 0, '425': 0, '430': 0}
+        analysis = {
+            '510': 0,
+            '515': 0,
+            '520': 0,
+            '525': 0,
+            '530': 0,
+            '410': 0,
+            '415': 0,
+            '420': 0,
+            '425': 0,
+            '430': 0
+        }
         for srckey in self.winLottos:
-            prizeHistory = {'1st':0, '2nd':0, '3rd':0, '4th':0, '5th':0}
+            prizeHistory = {'1st': 0, '2nd': 0, '3rd': 0, '4th': 0, '5th': 0}
             srcLotto = self.winLottos[srckey]
 
             for dstkey in self.winLottos:
@@ -132,9 +147,9 @@ class Lotto645Analysis:
                         if srcLotto[i] == dstLotto[j]:
                             matchCnt += 1
 
-                if matchCnt == 6:       # 1등
+                if matchCnt == 6:  # 1등
                     prizeHistory['1st'] += 1
-                elif matchCnt == 5:     # 2, 3등
+                elif matchCnt == 5:  # 2, 3등
                     is2nd = False
                     for j in range(len(srcLotto)):
                         if srcLotto[j] == dstLotto[6]:
@@ -142,9 +157,9 @@ class Lotto645Analysis:
                             prizeHistory['2nd'] += 1
                     if is2nd is False:
                         prizeHistory['3rd'] += 1
-                elif matchCnt == 4:     # 4등
+                elif matchCnt == 4:  # 4등
                     prizeHistory['4th'] += 1
-                elif matchCnt == 3:     # 5등
+                elif matchCnt == 3:  # 5등
                     prizeHistory['5th'] += 1
 
             if prizeHistory['5th'] > 30:
@@ -170,7 +185,7 @@ class Lotto645Analysis:
                 analysis['410'] += 1
 
         print(analysis)
-            
+
     def GenWinNumbers(self, min, max, len, cnt):
         _cnt = 0
         while True:
@@ -182,7 +197,7 @@ class Lotto645Analysis:
                     time.sleep(random.uniform(0, 3))
                     random.seed()
                     newNum = random.randrange(min, max + 1)
-   
+
                     isDuplicateNum = False
                     for j in pickNum:
                         if j == newNum:
@@ -201,22 +216,22 @@ class Lotto645Analysis:
                     continue
 
                 prize = self.CompareWithWinLottos(pickNum)
-                if prize[4] < 15 or prize[4] > 25:
+                if prize[4] < 15 or prize[4] > 25 or prize[1] > 0 or prize[0] > 0:
                     continue
 
                 _cnt += 1
                 print(pickNum)
                 print('합 {0} 출현횟수 {1}, 당첨횟수 1:{2}, 2:{3}, 3:{4}, 4:{5}, 5:{6}'.
-                      format(sum, self.winLottosSum[str(sum)], prize[0], prize[1], prize[2], prize[3], prize[4]))
+                      format(sum, self.winLottosSum[str(sum)], prize[0], prize[
+                          1], prize[2], prize[3], prize[4]))
 
             if _cnt == cnt:
                 break
 
-
 if __name__ == "__main__":
     lotto = Lotto645Analysis()
     #lotto.CompareWithEachWinNumbers()
-    #lotto.PrintMyLottoPrizeHistory([22, 2, 27, 19, 35, 37])
+    lotto.PrintMyLottoPrizeHistory([19, 32, 37, 40, 41, 43])
     #lotto.PrintMyLottoPrizeHistory([4, 35, 45, 25, 14, 1])
     #lotto.PrintMyLottoPrizeHistory([42, 7, 29, 1, 23, 22])
     #lotto.PrintMyLottoPrizeHistory([21, 20, 37, 39, 15, 1])
